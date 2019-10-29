67900d1f77d73677184d3459dc290143917ae5f0
Limpat Prananda <limpat.prananda@gmail.com>
2019-10-29 15:08:29 +0700

    sync and async request

--- app/src/main/java/com/limpatprananda/plugin/simpleretrofit/MainActivity.kt
+++ app/src/main/java/com/limpatprananda/plugin/simpleretrofit/MainActivity.kt
@@ -2,28 +2,73 @@ package com.limpatprananda.plugin.simpleretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.NetworkOnMainThreadException
+import android.os.StrictMode
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import timber.log.Timber
+import java.io.IOException
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    private var stringText: String = "Connecting to Internet!!!\n"
+    private val DELEYED_SECONDS: Int = 5
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val stringCall: Call<String> = service.getListRepositories("limpatprananda")
+        button.setOnClickListener {
+            stringText += "\nclicked.."
+            updateText()
+        }
+
+        syncButton.setOnClickListener {
+            stringText += "\nsync.."
+            updateText()
+            synchronousRequest()
+        }
+
+        asyncButton.setOnClickListener {
+            stringText += "\nasync.."
+            updateText()
+            asynchronousRequest()
+        }
+        resultText.text = stringText
+    }
+
+    fun updateText(){
+        resultText.text = stringText
+    }
+
+    fun synchronousRequest(){
+        val stringCall: Call<String> = service.getPersons(DELEYED_SECONDS)
+        val threadPolicy = StrictMode.ThreadPolicy.Builder()
+            .permitAll()
+            .build()
+        StrictMode.setThreadPolicy(threadPolicy)
+
+        val result = stringCall.execute().body()
+        stringText += "\n\n" + result
+        updateText()
+    }
+
+    fun asynchronousRequest(){
+        val stringCall: Call<String> = service.getPersons(DELEYED_SECONDS)
+
         stringCall.enqueue(object : Callback<String>{
             override fun onFailure(call: Call<String>, t: Throwable) {
-                resultText.text = "onFailure ${t.message}"
+                stringText += "onFailure ${t.message}"
+                updateText()
             }
 
             override fun onResponse(call: Call<String>, response: Response<String>) {
-                resultText.text = "onResponse ${response.body()}"
+                stringText += "\n\n" + response.body()
+                updateText()
             }
         })
     }
--- app/src/main/java/com/limpatprananda/plugin/simpleretrofit/Network.kt
+++ app/src/main/java/com/limpatprananda/plugin/simpleretrofit/Network.kt
@@ -1,19 +1,30 @@
 package com.limpatprananda.plugin.simpleretrofit
 
+import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.Field
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
+import java.util.concurrent.TimeUnit
 
-public interface GitHubService{
-    @GET("users/{user}/repos")
-    fun getListRepositories(@Path("user") user: String) : Call<String>
+public interface NetworkServices{
+    @GET("delayphp/index.php")
+    fun getPersons(@Query("seconds") seconds: Int) : Call<String>
 }
 
-val retrofit = Retrofit.Builder()
-    .baseUrl("https://api.github.com/")
-    .addConverterFactory(ScalarsConverterFactory.create())
+private val okHttpClient = OkHttpClient.Builder()
+    .connectTimeout(60, TimeUnit.SECONDS)
+    .readTimeout(60, TimeUnit.SECONDS)
+    .writeTimeout(60, TimeUnit.SECONDS)
     .build()
 
-val service = retrofit.create(GitHubService::class.java)
+val retrofit = Retrofit.Builder()
+    .baseUrl("https://www.bjtiport.co.id/tools/")
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .client(okHttpClient)
+    .build()
+
+val service = retrofit.create(NetworkServices::class.java)
