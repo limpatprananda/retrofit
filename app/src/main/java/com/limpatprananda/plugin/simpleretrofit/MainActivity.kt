39f02049aab90ce17a6a2d5e788c103fd13dfdde
Limpat Prananda <limpat.prananda@gmail.com>
2019-10-29 10:53:57 +0700

    convert string json to list object

--- app/src/main/java/com/limpatprananda/plugin/simpleretrofit/MainActivity.kt
+++ app/src/main/java/com/limpatprananda/plugin/simpleretrofit/MainActivity.kt
@@ -3,6 +3,10 @@ package com.limpatprananda.plugin.simpleretrofit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -23,8 +27,25 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onResponse(call: Call<String>, response: Response<String>) {
-                resultText.text = "onResponse ${response.body()}"
+                val responseString = response.body()
+
+                val moshi = Moshi.Builder()
+                    .add(KotlinJsonAdapterFactory())
+                    .build()
+                val type = Types.newParameterizedType(List::class.java, Repository::class.java)
+                val jsonAdapter = moshi.adapter<List<Repository>>(type)
+                val listRepositories = jsonAdapter.fromJson(responseString)
+
+                listRepositories?.get(0)?.full_name = "Dynamicly Change"
+
+                resultText.text = "onResponse ${jsonAdapter.toJson(listRepositories)}"
             }
         })
     }
 }
+
+data class Repository(
+    val id: Int,
+    val name: String,
+    var full_name: String
+)
