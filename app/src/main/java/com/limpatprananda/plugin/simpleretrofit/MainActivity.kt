5d7991fa8cd45f7b932024f8cae38d8d6f2354f1
Limpat Prananda <limpat.prananda@gmail.com>
2019-10-29 11:46:57 +0700

    convert retrofit with moshi to object

--- app/src/main/java/com/limpatprananda/plugin/simpleretrofit/MainActivity.kt
+++ app/src/main/java/com/limpatprananda/plugin/simpleretrofit/MainActivity.kt
@@ -3,10 +3,8 @@ package com.limpatprananda.plugin.simpleretrofit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -20,32 +18,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val stringCall: Call<String> = service.getListRepositories("limpatprananda")
-        stringCall.enqueue(object : Callback<String>{
-            override fun onFailure(call: Call<String>, t: Throwable) {
+        val call = service.getListRepositories("limpatprananda")
+        call.enqueue(object : Callback<List<Repository>>{
+            override fun onFailure(call: Call<List<Repository>>, t: Throwable) {
                 resultText.text = "onFailure ${t.message}"
             }
 
-            override fun onResponse(call: Call<String>, response: Response<String>) {
-                val responseString = response.body()
-
-                val moshi = Moshi.Builder()
-                    .add(KotlinJsonAdapterFactory())
-                    .build()
-                val type = Types.newParameterizedType(List::class.java, Repository::class.java)
-                val jsonAdapter = moshi.adapter<List<Repository>>(type)
-                val listRepositories = jsonAdapter.fromJson(responseString)
-
-                listRepositories?.get(0)?.full_name = "Dynamicly Change"
-
-                resultText.text = "onResponse ${jsonAdapter.toJson(listRepositories)}"
+            override fun onResponse(
+                call: Call<List<Repository>>,
+                response: Response<List<Repository>>
+            ) {
+                val listRepositories = response.body()
+                listRepositories?.get(0)?.fullName = "Dynamicly Change"
+                resultText.text = "onResponse ${listRepositories.toString()}"
             }
         })
     }
 }
 
+@JsonClass(generateAdapter = true)
 data class Repository(
     val id: Int,
     val name: String,
-    var full_name: String
+    @Json(name = "full_name")
+    var fullName: String
 )
--- app/src/main/java/com/limpatprananda/plugin/simpleretrofit/Network.kt
+++ app/src/main/java/com/limpatprananda/plugin/simpleretrofit/Network.kt
@@ -1,19 +1,25 @@
 package com.limpatprananda.plugin.simpleretrofit
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 public interface GitHubService{
     @GET("users/{user}/repos")
-    fun getListRepositories(@Path("user") user: String) : Call<String>
+    fun getListRepositories(@Path("user") user: String) : Call<List<Repository>>
 }
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
 
 val retrofit = Retrofit.Builder()
     .baseUrl("https://api.github.com/")
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .build()
 
 val service = retrofit.create(GitHubService::class.java)
